5e89bacb923f400fa1b2db3a5f7fe03bc6c781bd
alrshdn <mohammed.alrashdan.yes@gmail.com>
2022-11-16 19:34:45 +0300

    Added game image source.

---
+++ src/game_image.py
@@ -0,0 +1,24 @@
+from PIL.ImageTk import PhotoImage
+import os
+
+
+class GameImage():
+
+    def __init__(self):
+        self.start_directory = os.getcwd()
+
+        # Ran from src directory
+        if self.start_directory.split('\\')[-1] == "src":
+            self.image_directory = f'..\\static\\images'
+        # Ran from Pacman directory
+        else:
+            self.image_directory = f'{self.start_directory}\\static\\images'
+
+        self.game_images = dict()
+
+        for image in os.listdir( self.image_directory ):
+            self.game_images[ image[:-4] ] = PhotoImage(file= f'{self.image_directory}\\{image}')
+
+    def return_image(self, image) -> PhotoImage:
+        ''' Returns the image from the image_directory given the image argument. '''
+        return self.game_images[image]
