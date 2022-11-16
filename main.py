2c4f3dad60c7a94b2e073859485f38bc309d5890
alrshdn <mohammed.alrashdan.yes@gmail.com>
2022-11-16 11:26:55 +0300

    test

---
+++ main.py
@@ -0,0 +1,25 @@
+class Entity:
+    # pos, name, image, movement, speed
+    pass
+
+
+class PacMan(Entity):
+    # score, lives,
+    pass
+
+
+class Ghost(Entity):
+    # states
+    pass
+
+
+class Board:
+    # walls, empty
+    pass
+
+
+class Points:
+    # small, big
+    pass
+
+
